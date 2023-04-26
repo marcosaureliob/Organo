db4df16ce2641c0d1e60fb75e476d83e0669f296
Marcos Aurelio <77805407+marcosaureliob@users.noreply.github.com>
2023-04-26 15:29:52 -0300

    projeto final

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,17 +1,33 @@
-import './colaborador.css'
-import { AiFillCloseCircle } from 'react-icons/ai';
+import "./colaborador.css";
+import { AiFillCloseCircle, AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
-const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
-    return (<div className="colaborador">
-        <AiFillCloseCircle size={25} className="deletar" onClick={() => aoDeletar(colaborador.id)} />
-        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
-            <img src={colaborador.imagem} alt={colaborador.nome} />
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+  function favoritar() {
+    aoFavoritar(colaborador.id);
+  }
+  return (
+    <div className="colaborador">
+      <AiFillCloseCircle
+        size={25}
+        className="deletar"
+        onClick={() => aoDeletar(colaborador.id)}
+      />
+      <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+        <img src={colaborador.imagem} alt={colaborador.nome} />
+      </div>
+      <div className="rodape">
+        <h4>{colaborador.nome}</h4>
+        <h5>{colaborador.cargo}</h5>
+        <div className="favorito">
+          {colaborador.favorito ? (
+            <AiFillHeart color="#ff0000" size={25} onClick={favoritar} />
+          ) : (
+            <AiOutlineHeart size={25} onClick={favoritar} />
+          )}
         </div>
-        <div className="rodape">
-            <h4>{colaborador.nome}</h4>
-            <h5>{colaborador.cargo}</h5>
-        </div>
-    </div>)
-}
+      </div>
+    </div>
+  );
+};
 
-export default Colaborador
+export default Colaborador;
